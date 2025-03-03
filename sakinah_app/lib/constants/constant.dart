36e0f668b4b6b367646cbeb7bac3880f0d422bf8
shazaniyu55 <shazaniyu@gmail.com>
2025-03-03 11:05:07 +0100

    splashscreen

---
+++ sakinah_app/lib/constants/constant.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: constant_identifier_names
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// const color
+const COLOR_PRIMARY = Color.fromRGBO(251, 38, 19, 1);
+const COLOR_ACCENT =  Color.fromARGB(253, 251, 156, 13);
+const COLOR_BACKGROUND_DARK = Color(0xFF171822);
+const COLOR_BACKGROUND = Colors.white;
+const COLOR_BACKGROUND_LIGHT = Color(0xFFF1F3F6);
+const COLOR_BLUE = Colors.blue;
+
+const Color primary = Colors.black;
+const  red = Colors.red;
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color grey = Colors.grey;
+const Color green = Colors.green;
+var appBgGradient = const LinearGradient(
+  begin: Alignment(0.0, 0.0),
+  end: Alignment(0, -1),
+  colors: [Color(0xffDB9B3A),Color(0xffFE8664)],
+);
+// Custom Fonts
+const TextStyle customTextStyle = TextStyle(fontFamily: 'Montserrat',fontSize: 16.0,fontWeight: FontWeight.normal,);
+final TextStyle customBoldTextStyle = customTextStyle.copyWith(fontWeight: FontWeight.bold,);
+// Example: Custom Google Font
+final TextStyle customGoogleFontStyle = GoogleFonts.montserrat(textStyle: customTextStyle,);
+final TextStyle customGoogleFontBoldStyle = GoogleFonts.montserrat(textStyle: customBoldTextStyle,);
+
---
+++ sakinah_app/lib/screens/splashscreen.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sakinah_app/constants/constant.dart';
+import 'package:sakinah_app/helper/navigation.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual);
+    Future.delayed(const Duration(seconds: 6), () {
+      changeScreenReplacement(context,  Container());
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [COLOR_BACKGROUND, Color.fromARGB(255, 219, 216, 216)],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("images/logo.png", width: 100, height: 100,)
+
+          ],
+        ),
+      ),
+    );
+  }
+}
